c63dddbed2cd76df4b2b3ae3e6bdb9bb51922a35
camilo9090 <camilou9090@gmail.com>
2024-11-21 14:36:33 -0500

    se finalizo el projecto

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,9 +2,20 @@
 import { BriefcaseIcon } from "@heroicons/react/24/solid";
 import Card from "./Card";
 import { projects } from "../data/data";
+import { useInView } from "react-intersection-observer";
 export default function Projects() {
 
+  const { ref: titleProjects, inView: initialProject } = useInView({
 
+    triggerOnce: true,
+    initialInView: false
+  });
+
+  const { ref: ContentProjects, inView: initialContent } = useInView({
+
+    triggerOnce: true,
+    initialInView: false
+  });
   return (
     <section id="projects">
 
@@ -18,8 +29,9 @@ export default function Projects() {
 
 
             <div
+              ref={titleProjects}
               id="proyectos"
-              className={`flex text-4xl justify-center gap-3 mb-10`}>
+              className={`flex text-4xl opacity-0 justify-center gap-3 mb-10 ${initialProject&&'opacity-100 md:animate-fade-down animate-none md:animate-once md:animate-ease-in md:animate-normal'}`}>
 
               <h2 className="font-black"> Proyectos</h2>
 
@@ -28,7 +40,9 @@ export default function Projects() {
             </div>
 
 
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 w-auto items-start  gap-5 ">
+            <div 
+            ref={ContentProjects}
+            className={`grid opacity-0 md:grid-cols-2 lg:grid-cols-3 w-auto items-start  gap-5 ${initialContent&&'opacity-100 md:animate-fade-left animate-none md:animate-once md:animate-ease-in md:animate-normal'}`}>
 
 
               {
